dfc87f349b3722de967db0844c9cc9ff048d2253
karanjotsingh-max <kjsingh@mitaoe.ac.in>
2024-06-16 16:47:06 +0530

    py cheat sheet added

--- test.py
+++ test.py
@@ -1,16 +1,24 @@
-# m=1
-# while(m==1):
+# # m=1
+# # while(m==1):
     
-#     m=input(("Enter Your Name: "))
-#     n,t="inavihs","narak"
+# #     m=input(("Enter Your Name: "))
+# #     n,t="inavihs","narak"
 
-#     if(m==n[::-1]):
-#       print(m, "your lover name is ",t[::-1])
-#     elif(m==t[::-1]):
-#        print(m, "your lover name is ",n[::-1])
-#     else:
-#       print("Sorry ",m,"!! Our Database has no data for you")
-#     m=int(input("Press 1 to run / 0 to exit: "))
+# #     if(m==n[::-1]):
+# #       print(m, "your lover name is ",t[::-1])
+# #     elif(m==t[::-1]):
+# #        print(m, "your lover name is ",n[::-1])
+# #     else:
+# #       print("Sorry ",m,"!! Our Database has no data for you")
+# #     m=int(input("Press 1 to run / 0 to exit: "))
 
-for i in range(10,0,-1):
-    print(i)
+# for i in range(10,0,-1):
+#     print(i)
+arr=[]
+arr.insert(0,2)
+arr.insert(0,2)
+arr.insert(1,3)
+print(arr[1])
+arr2=[4,5]
+arr=arr+arr2
+print(0%10)
--- test2.py
+++
@@ -1,2 +0,0 @@
-A=[int(A) for A in input().split()]
-print(A)
